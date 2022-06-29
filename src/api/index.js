f71dfdcfd65e83f2fab28daf2a608f7e8f55b313
James Nguyen <nguyentri1307@gmail.com>
2022-06-28 22:31:18 -0700

    deploy

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+
+
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': '018ce96ac0msha2e7b7a593a4663p105310jsn2f831843523a',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/weather', {
+        params: { lat: lat, lon: lng },
+        headers: {
+          'X-RapidAPI-Key': '018ce96ac0msha2e7b7a593a4663p105310jsn2f831843523a',
+          'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+    },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
